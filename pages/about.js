31464300d89b53d146ab4c0a955b2160359e18cf
laserany <dotachessfan@outlook.com>
2021-01-31 15:25:48 -0600

    first commit

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Meta from '../components/Meta'
+const about = () => {
+  return (
+    <div>
+      <Meta title='about' />
+      <h1>about MEeeee</h1>
+    </div>
+  )
+}
+
+export default about
